2f9bb0fa1ab2f24eda90e45f8abbb1ef3b30105d
JordanBeziau <jordanbeziau@gmail.com>
2018-03-08 09:34:16 +0100

    New controller

--- first/src/AppBundle/Controller/DefaultController.php
+++ first/src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,11 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+    /**
+     * @Route("/blog", name="blogpage")
+     */
+  public function blogAction() {
+    return $this->render('@App/blog.html.twig', []);
+  }
 }
